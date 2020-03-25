0de1a4346d6d917cb49da3f3df51463a952aa4e9
Johan Sällström <johansallstrom7@outlook.com>
2020-03-25 15:03:26 +0100

    Second commit

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json()); // adding a piece of middle ware to open up features
+app.use(express.json());
 app.use(bodyParser.json());
 
-const PORT = 3001; //  the port which you will use
-
-// REMOVE ALL COMMENTS!!!
+const PORT = 3001;
 
 app.listen(PORT, () => {
      console.log(`Example app listening on localhost:${PORT}`);
@@ -28,11 +26,6 @@ app.post("/", (req, res) => {
      res.status(200).json(result);
 });
 
-// Might have to install cors here so the React app can access it, else we get an
-// security error
-
-// make it more about, game won if, return result and make this code clearer for you
-// so you can explain it better, change name of function names also
 function playGame(changeDoor) {
      const allDoors = [0, 1, 2];
 
